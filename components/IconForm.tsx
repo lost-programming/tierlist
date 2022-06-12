7d19ca563474218e5c8305d457b8ac2e10466209
dnwnvus <dnwnvus@gmail.com>
2022-06-12 22:09:14 +0900

    dnd init

--- components/IconForm.tsx
+++ components/IconForm.tsx
@@ -1,18 +1,40 @@
+import { useSortable } from "@dnd-kit/sortable"
+import { CSS } from '@dnd-kit/utilities'
+
 interface IconFormType {
   id: any
   name: any
+  img: string
 }
 
-const IconForm = ({ id, name }: IconFormType) => {
+const IconForm = ({ id, name, img }: IconFormType) => {
+
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transition,
+    transform,
+    isDragging,
+  } = useSortable({ id: id })
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+    border: '2px solid black',
+    marginBottom: 10,
+    marginTop: 10,
+    opacity: isDragging ? 0.5 : 1
+  }
+
   return (
     <div
-      style={{
-        border: '2px solid black',
-        marginBottom: 10,
-        marginTop: 10
-      }}
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
     >
-      {name}
+      <img src={img} style ={{ width: 100, height: 100 }}/>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,47 +1,70 @@
 import '../styles/globals.css'
 import { useState } from 'react'
 import type { AppProps } from 'next/app'
-import { DndContext } from '@dnd-kit/core'
-import { SortableContext } from '@dnd-kit/sortable'
+import { closestCenter, DndContext, PointerSensor, useSensor } from '@dnd-kit/core'
+import { arrayMove, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 import IconForm from '../components/IconForm'
 
 const TierList = ({ Component, pageProps }: AppProps) => {
   const [items, setItems] = useState([
     {
-      id: 1,
-      name: 'Chicken',
-      image: 'https://kfcapi.inicis.com/kfcs_api_img/KFCS/goods/DL_2175114_20220106094224864.png'
+      id: '1',
+      name: '치킨',
+      img: 'https://kfcapi.inicis.com/kfcs_api_img/KFCS/goods/DL_2175114_20220106094224864.png'
     },
     {
-      id: 2,
-      name: 'Pizza',
-      image: 'https://img.hankyung.com/photo/202108/99.26501439.1-1200x.jpg'
+      id: '2',
+      name: '피자',
+      img: 'https://img.hankyung.com/photo/202108/99.26501439.1-1200x.jpg'
     },
     {
-      id: 3,
-      name: 'Steak',
-      image: 'https://static.wtable.co.kr/image/production/service/recipe/873/1c52a4fd-68fb-458f-aa6c-cf3537d674df.jpg'
+      id: '3',
+      name: '돈가스',
+      img: 'https://img.sbs.co.kr/newsnet/etv/upload/2014/01/10/30000347969_1280.jpg'
+    },
+    {
+      id: '4',
+      name: '족발',
+      img: 'http://image.auction.co.kr/itemimage/23/f5/9d/23f59d9426.jpg'
+    },
+    {
+      id: '5',
+      name: '보쌈',
+      img: 'https://recipe1.ezmember.co.kr/cache/recipe/2017/07/20/ecdefb73ec9113b40fcb123183e4eeb51.jpg'
     }
   ])
 
-  const sensors = useSensors()
+  const sensors = [useSensor(PointerSensor)]
+
+  const handleDragEnd = ({ active, over }: any) => {
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.findIndex(item => item.id === active.id)
+        const newIndex = items.findIndex(item => item.id === over.id)
+  
+        return arrayMove(items, oldIndex, newIndex)
+      })
+    }
+  }
 
   return (
     <div
       style={{
         margin: 'auto',
+        width: 100,
         textAlign: 'center'
       }}
     >
       <DndContext
         sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
       >
-        <SortableContext>
-          {
-            items.map(
-              item => <IconForm {...item} key={item.id} />
-            )
-          }
+        <SortableContext 
+          items={items.map(item => item.id)}
+          strategy={verticalListSortingStrategy}
+        >
+          {items.map(item => <IconForm {...item} key={item.id} />)}
         </SortableContext>
       </DndContext>
       <Component {...pageProps} />
