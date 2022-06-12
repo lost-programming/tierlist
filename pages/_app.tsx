75371fe7baae3a38e827114d91309779a9f5be3e
dnwnvus <dnwnvus@gmail.com>
2022-06-12 16:25:31 +0900

    Yarn.lock 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,52 @@
 import '../styles/globals.css'
+import { useState } from 'react'
 import type { AppProps } from 'next/app'
+import { DndContext } from '@dnd-kit/core'
+import { SortableContext } from '@dnd-kit/sortable'
+import IconForm from '../components/IconForm'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const TierList = ({ Component, pageProps }: AppProps) => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      name: 'Chicken',
+      image: 'https://kfcapi.inicis.com/kfcs_api_img/KFCS/goods/DL_2175114_20220106094224864.png'
+    },
+    {
+      id: 2,
+      name: 'Pizza',
+      image: 'https://img.hankyung.com/photo/202108/99.26501439.1-1200x.jpg'
+    },
+    {
+      id: 3,
+      name: 'Steak',
+      image: 'https://static.wtable.co.kr/image/production/service/recipe/873/1c52a4fd-68fb-458f-aa6c-cf3537d674df.jpg'
+    }
+  ])
+
+  const sensors = useSensors()
+
+  return (
+    <div
+      style={{
+        margin: 'auto',
+        textAlign: 'center'
+      }}
+    >
+      <DndContext
+        sensors={sensors}
+      >
+        <SortableContext>
+          {
+            items.map(
+              item => <IconForm {...item} key={item.id} />
+            )
+          }
+        </SortableContext>
+      </DndContext>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
-export default MyApp
+export default TierList;
